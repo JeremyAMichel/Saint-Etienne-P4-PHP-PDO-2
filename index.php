726dd3ef2ceb9bb096725f31b438eb91a527fa35
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2024-12-10 17:06:21 +0100

    setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospitale</title>
+    <link rel="stylesheet" href="./assets/styles.css">
+</head>
+<body>
+    <h1>Hospitale</h1>
+    <div class="wrapper">
+        <a href="./liste-patients.php">Liste Patients</a>
+        <a href="./ajout-patient.php">Ajouter un Patient</a>
+        <a href="./liste-rendezvous.php">Liste Rendez-vous</a>
+        <a href="./ajout-rendezvous.php">Ajouter un Rendez-vous</a>
+
+    </div>
+</body>
+</html>
